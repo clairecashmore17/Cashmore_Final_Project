b92271cd11521f2955259fbf242d2d10fa65f349
Claire Cashmore <clairecashmore2017@gmail.com>
2025-04-14 16:48:02 -0400

    refeactored buffer structure

--- FinalProject/Final_Project.js
+++ FinalProject/Final_Project.js
@@ -8,17 +8,43 @@ var gl;
 
 var fColorLocation;
 var matrixLocation;
+var vertexColors = [
+    vec4(0.0, 0.0, 0.0, 1.0),  // black  0
+    vec4(1.0, 0.0, 0.0, 1.0),  // red  1
+    vec4(1.0, 1.0, 0.0, 1.0),  // yellow  2
+    vec4(0.0, 1.0, 0.0, 1.0),  // green  3
+    vec4(0.0, 0.0, 1.0, 1.0),  // blue  4
+    vec4(1.0, 0.0, 1.0, 1.0),  // magenta  5
+    vec4(0.0, 1.0, 1.0, 1.0),  // cyan  6
+    vec4(1.0, 1.0, 1.0, 1.0),  // white  7
+    vec4(0.6, .39, 0.0, 1.0) // brown  8
+];
 var colorsArray = [];
-// delay (or n) is the number of frames we load at
-var delay = 10;
+function colorBuffer(cBuffer, colorIndex,vColor){
+    colorsArray = [vertexColors[colorIndex], vertexColors[colorIndex], vertexColors[colorIndex], vertexColors[colorIndex], vertexColors[colorIndex]];
+   
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
 
-//cindex is how we will change the colors
-var cindex = 1.0;
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
 
-var fps = 5;
+};
 
-// r b g values (a will always be 1)
-var r, b, g;
+function positionBuffer(positionBuffer,vertices,vPosition){
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+
+    // // Associate out shader variables with our data buffer
+    // var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+};
+
+
 //Run this once the page has loaded
 window.onload = function init() {
 
@@ -35,19 +61,9 @@ window.onload = function init() {
     //  Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
-
-
-    var vertexColors = [
-        vec4(0.0, 0.0, 0.0, 1.0),  // black  0
-        vec4(1.0, 0.0, 0.0, 1.0),  // red  1
-        vec4(1.0, 1.0, 0.0, 1.0),  // yellow  2
-        vec4(0.0, 1.0, 0.0, 1.0),  // green  3
-        vec4(0.0, 0.0, 1.0, 1.0),  // blue  4
-        vec4(1.0, 0.0, 1.0, 1.0),  // magenta  5
-        vec4(0.0, 1.0, 1.0, 1.0),  // cyan  6
-        vec4(1.0, 1.0, 1.0, 1.0),  // white  7
-        vec4(0.6, .39, 0.0, 1.0) // brown  8
-    ];
+    var vColor = gl.getAttribLocation(program, "vColor");
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+   
     //Vertices needed to make H
     var window_vertices = [
         vec2(-.25, .25),
@@ -66,16 +82,10 @@ window.onload = function init() {
     matrixLocation = gl.getUniformLocation(program, 'u_matrix');
     colorsArray = [vertexColors[0], vertexColors[0], vertexColors[0], vertexColors[0], vertexColors[0]];
 
-
+    // Set first Color Buffer
     var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
-
-
+    colorBuffer(cBuffer, 0,vColor);
+ 
     /*********************** Create Window ***********************/
     // Degine vertices for window
     var window_vertices = [
@@ -100,42 +110,21 @@ window.onload = function init() {
 
 
 
-    //Lamp Base Color
-    colorsArray = [vertexColors[4], vertexColors[7], vertexColors[4], vertexColors[4], vertexColors[4]];
-
-
-    var cBufferSky = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBufferSky);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
+    //Sky Color
+    colorBuffer(cBuffer, 4,vColor);
+    
     //load into the GPU
-    var bufferIDInnerMatrix = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferIDInnerMatrix);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(window_vertices), gl.STATIC_DRAW);
-
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    var bufferID = gl.createBuffer();
+    positionBuffer(bufferID,window_vertices,vPosition);
+  
 
     // Render Inner frame
     render(innerFrameMatrix, window_vertices.length, "triangle_fan");
+   
+    
     //Lamp Base Color
-    colorsArray = [vertexColors[0], vertexColors[0], vertexColors[0], vertexColors[0], vertexColors[0]];
 
-
-    var cBufferWindowOutlines = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBufferWindowOutlines);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
+    colorBuffer(cBuffer, 0,vColor);
 
     //Create outer frame
     var outerFrameMatrix = mat4();
@@ -143,17 +132,11 @@ window.onload = function init() {
     outerFrameMatrix = translate(-.5, .25, 0);
     outerFrameMatrix = mult(outerFrameMatrix, scalem(1.25, 1.75, 1));
 
-    // Create second buffer for 2nd H
-    var bufferOuterFrame = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferOuterFrame);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(window_vertices), gl.STATIC_DRAW);
+ // add outer frame to buffer
+    positionBuffer(bufferID,window_vertices,vPosition);
+   
 
-
-    //Associate our shade vars again, but for 2nd H?...
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    // Render H2
+    // Render Outer Frame
     render(outerFrameMatrix, window_vertices.length, "line_strip");
 
 
@@ -164,46 +147,24 @@ window.onload = function init() {
     windowHorizontal = scalem(1, 0, 0);
     windowHorizontal = mult(windowHorizontal, translate(-.5, -.3, 0));
 
-
     // Create a buffer object, initialize it, and associate it with the
-    //load into the GPU
-    var horizontalWindowBufffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, horizontalWindowBufffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(window_bar_vertices), gl.STATIC_DRAW);
+    positionBuffer(bufferID,window_bar_vertices,vPosition);
 
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
 
     // Render H3
     render(windowHorizontal, window_bar_vertices.length, "line_strip");
 
 
     //Create verticalWindowLine
-    //var windowVertical = mat4();
-
-    // translate the matrix 
-    // windowHorizontal = translate(-.5, 0.5, 0);
-    // use mult to then rotate after the translation
-    // windowHorizontal = scalem(4,0, 0);
     windowHorizontal = rotate(90, 0, 0, 1);
     windowHorizontal = mult(windowHorizontal, scalem(2.3, 1, 1));
     windowHorizontal = mult(windowHorizontal, translate(.02, .25, 0));
 
     // Create a buffer object, initialize it, and associate it with the
     //load into the GPU
-    var verticalWindowBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, verticalWindowBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(window_bar_vertices), gl.STATIC_DRAW);
+   // positionBuffer(bufferID,window_bar_vertices,vPosition);
 
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
+    
     // Render H4
     render(windowHorizontal, window_bar_vertices.length, "line_strip");
 
@@ -221,17 +182,7 @@ window.onload = function init() {
         vec2(0, .25),
     ];
 
-    //Set table color
-    colorsArray = [vertexColors[8], vertexColors[8], vertexColors[8], vertexColors[8], vertexColors[8]];
-
-
-    var cBuffer2 = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer2);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
+    colorBuffer(cBuffer, 8,vColor);
 
     //Create tabletop
     var tableTopMatrix = mat4();
@@ -246,33 +197,16 @@ window.onload = function init() {
 
     // Create a buffer object, initialize it, and associate it with the
     //load into the GPU
-    var tableBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, tableBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(table_vertices), gl.STATIC_DRAW);
+    positionBuffer(bufferID,table_vertices,vPosition);
 
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    // Render H4
+    // Render Table
     render(tableTopMatrix, table_vertices.length, "triangle_fan");
 
     // create table leg
     var tableLegMatrix = mult(tableTopMatrix, rotate(90, 0, 0, 1));
     tableLegMatrix = mult(tableLegMatrix, translate(-.45, -.15, 0));
     tableLegMatrix = mult(tableLegMatrix, scalem(2.5, .35, 1));
-    var tableLegBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, tableLegBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(table_vertices), gl.STATIC_DRAW);
 
-
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
 
     // Render table leg
     render(tableLegMatrix, table_vertices.length, "triangle_fan");
@@ -292,15 +226,9 @@ window.onload = function init() {
 
     ]
 
-    //Lamp Base Color
-    colorsArray = [vertexColors[0], vertexColors[0], vertexColors[0], vertexColors[0], vertexColors[0]];
+    colorBuffer(cBuffer, 0,vColor);
 
-
-    var cBuffer3 = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer3);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
+    
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
@@ -316,37 +244,19 @@ window.onload = function init() {
 
     // Create a buffer object, initialize it, and associate it with the
     //load into the GPU
-    var lampBaseBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, lampBaseBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(lampBaseVertices), gl.STATIC_DRAW);
+    positionBuffer(bufferID,lampBaseVertices,vPosition);
 
 
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    // Render H4
+  
+    // Render Lamp Base
     render(lampbaseMatrix, lampBaseVertices.length, "triangle_fan");
 
     lampbaseMatrix = mult(lampbaseMatrix, rotate(180, 0, 0, 1));
     lampbaseMatrix = mult(lampbaseMatrix, translate(-.5, -.25, 0));
     render(lampbaseMatrix, lampBaseVertices.length, "triangle_fan");
 
-
-
     // Lamp Glass colors
-    //Lamp Base Color
-    colorsArray = [vertexColors[5], vertexColors[5], vertexColors[5], vertexColors[5], vertexColors[5]];
-
-
-    var cBuffer4 = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer4);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
+    colorBuffer(cBuffer, 5,vColor);
 
     // Create lampGlass
 
@@ -357,19 +267,8 @@ window.onload = function init() {
     // use mult to then rotate after the translation
 
     lampGlassMatrix = mult(lampGlassMatrix, scalem(.4, .75, 1));
-    // lampbaseMatrix = mult(lampGlassMatrix,translate(-.72, -.25, 0));
-
-    // Create a buffer object, initialize it, and associate it with the
-    //load into the GPU
-    var lampGlassBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, lampGlassBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(window_vertices), gl.STATIC_DRAW);
-
-
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
+   
+    positionBuffer(bufferID,window_vertices,vPosition);
 
     // Render H4
     render(lampGlassMatrix, window_vertices.length, "line_strip");
@@ -378,13 +277,7 @@ window.onload = function init() {
 
 
 function render(matrix, n, type,) {
-    //gl.clear( gl.COLOR_BUFFER_BIT );
-    //increase theta to rotate shape
-
-
-    //gl.clear( gl.COLOR_BUFFER_BIT );
-
-
+   
     // Set the matrix uniform in the shader
     gl.uniformMatrix4fv(matrixLocation, false, flatten(matrix));
     //draw the two H's
